f6b7477bcf657cf98b910e59f004a6a961379abd
igless7 <98765638+igless7@users.noreply.github.com>
2024-02-05 12:31:06 -0600

    insert api call

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
+function updateWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#speed");
+    let timeElement = document.querySelector("#date");
+    let date = new Date(response.data.time*1000);
+    timeElement.innerHTML = formatDate(date);
+
+    temperatureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.wind.speed}km/h`;
+    console.log(response.data);
+}
+
+function formatDate(date){
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if(minutes<10){
+        minutes = `0${minutes}`;
+    }
+
+   return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+    let apiKey = `58ce410f0od07a6a15e649f48tfbd393`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeather);
+    console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-
+    searchCity(searchInput.value);
 }
 
-let apiKey = `58ce410f0od07a6a15e649f48tfbd393`;
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
+
+searchCity("paris");
